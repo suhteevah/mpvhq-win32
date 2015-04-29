406eb03bea4b34e1531a0d83878cf59f2ace0789
suhteevah <suhteevah@gmail.com>
2015-04-29 06:17:54 -0700

    newest commit from mpvhq

---
+++ buildmpvhq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Cloning mpvhq from github"
+
+cd /home/sativa/mpvhq
+
+git pull
+
+echo "Configuring mpvhq"
+
+python ./bootstrap.py
+
+export PATH=/opt/mxe/usr/bin/:$PATH
+DEST_OS=win32 TARGET=i686-w64-mingw32.static ./waf configure
+
+echo "Building mpvhq"
+
+./waf build
+
+echo "Moving files into home"
+cd /home/sativa/mpvhq/build
+
+cp mpv.com /home/sativa/mpv.com
+cp mpv.exe /home/sativa/mpv.exe
+
+cd /home/sativa/
+
+upx mpv.exe
+upx mpv.com
+
+echo "Finished"
